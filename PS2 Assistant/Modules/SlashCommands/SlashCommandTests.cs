88ab8ad3463a0acac5907ff34bbdde49b86a1ab7
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-08-07 15:27:25 +0200

    Added test ping command

---
+++ PS2 Assistant/Modules/SlashCommands/SlashCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+
+namespace PS2_Assistant.Modules.SlashCommands
+{
+    
+    [DontAutoRegister]
+    public class SlashCommandTests : InteractionModuleBase<SocketInteractionContext>
+    {
+        [DefaultMemberPermissions(Discord.GuildPermission.Administrator)]
+        [SlashCommand("ping", "Testing version of the /ping command, only available in this guild")]
+        public async Task TestPing() =>
+            await RespondAsync("Pong");
+    }
+}
