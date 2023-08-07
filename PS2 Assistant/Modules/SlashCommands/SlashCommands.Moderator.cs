090dfbbec4215185d110d0818349122a790c8407
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-08-07 15:35:08 +0200

    Added set role/channel commands to IF

---
+++ PS2 Assistant/Modules/SlashCommands/SlashCommands.Moderator.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Newtonsoft.Json.Linq;
+using Serilog.Events;
+
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+using PS2_Assistant.Attributes;
+using PS2_Assistant.Attributes.Preconditions;
+
+namespace PS2_Assistant.Modules.SlashCommands
+{
+    public partial class SlashCommands
+    {
+        [NeedsDatabaseEntry]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.ManageGuild)]
+        [SlashCommand("set-log-channel", "Sets the channel where this bot's log messages will be sent")]
+        public async Task SetLogChannel(
+            [TargetChannelPermission(_channelWritePermissions)]
+            [Summary(description: "Sets the log channel")]
+            ITextChannel logChannel)
+        {
+            await DeferAsync();
+
+            Channels channels = (await _guildDb.GetGuildByGuildIdAsync(Context.Guild.Id))!.Channels;
+            channels.LogChannel = logChannel.Id;
+            _guildDb.SaveChanges();
+
+            _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "Log channel set to {LogChannelId}", logChannel.Id);
+            await FollowupAsync($"Log channel set to <#{logChannel.Id}>");
+        }
+
+        [NeedsDatabaseEntry]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.ManageGuild)]
+        [SlashCommand("set-welcome-channel", "Sets the channel where new users will be greeted by the bot")]
+        public async Task SetWelcomeChannel(
+            [TargetChannelPermission(_channelWritePermissions)]
+            [Summary(description: "Sets the welcome channel")]
+            ITextChannel welcomeChannel)
+        {
+            await DeferAsync();
+
+            Channels channels = (await _guildDb.GetGuildByGuildIdAsync(Context.Guild.Id))!.Channels;
+            channels.WelcomeChannel = welcomeChannel.Id;
+            _guildDb.SaveChanges();
+
+            _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "Welcome channel set to {WelcomeChannelId}", welcomeChannel.Id);
+            await FollowupAsync($"Welcome channel set to <#{welcomeChannel.Id}>");
+        }
+
+        [NeedsDatabaseEntry]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.ManageGuild)]
+        [SlashCommand("set-member-role", "Sets the role users will get if their character is in the outfit represented by this server")]
+        public async Task SetMemberRole(
+            [Summary(description: "Sets the member role")]
+            IRole memberRole)
+        {
+            await DeferAsync();
+
+            Roles roles = (await _guildDb.GetGuildByGuildIdAsync(Context.Guild.Id))!.Roles;
+            roles.MemberRole = memberRole.Id;
+            _guildDb.SaveChanges();
+
+            _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "Member role set to {MemberRoleId}", memberRole.Id);
+            await FollowupAsync($"Member role set to {memberRole.Mention}");
+
+            SocketRole? botRole = Context.Guild.CurrentUser.Roles.FirstOrDefault(x => x.IsManaged);
+            if (memberRole.Position > botRole?.Position)
+            {
+                _logger.SendLog(LogEventLevel.Warning, Context.Guild.Id, "Bot doesn't have the right permissions to give role {MemberRoleId} to users", memberRole.Id);
+                await FollowupAsync($"The bot won't be able to give role {memberRole.Mention} to users, because it outranks the bot's role. Please go to `Server Settings -> Roles` and make sure that the {botRole.Mention} role is higher on the list than the {memberRole.Mention} role.", allowedMentions: AllowedMentions.None);
+            }
+        }
+
+        [NeedsDatabaseEntry]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.ManageGuild)]
+        [SlashCommand("set-non-member-role", "Sets the role users will get if their character isn't in the outfit represented by this server")]
+        public async Task SetNonMemberRole(
+            [Summary(description: "Sets the non-member role")]
+            IRole nonMemberRole
+            )
+        {
+            await DeferAsync();
+
+            Roles roles = (await _guildDb.GetGuildByGuildIdAsync(Context.Guild.Id))!.Roles;
+            roles.NonMemberRole = nonMemberRole.Id;
+            _guildDb.SaveChanges();
+
+            _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "Member role set to {MemberRoleId}", nonMemberRole.Id);
+            await FollowupAsync($"Member role set to {nonMemberRole.Mention}");
+
+            SocketRole? botRole = Context.Guild.CurrentUser.Roles.FirstOrDefault(x => x.IsManaged);
+            if (nonMemberRole.Position > botRole?.Position)
+            {
+                _logger.SendLog(LogEventLevel.Warning, Context.Guild.Id, "Bot doesn't have the right permissions to give role {MemberRoleId} to users", nonMemberRole.Id);
+                await FollowupAsync($"The bot won't be able to give role {nonMemberRole.Mention} to users, because it outranks the bot's role. Please go to `Server Settings -> Roles` and make sure that the {botRole.Mention} role is higher on the list than the {nonMemberRole.Mention} role.", allowedMentions: AllowedMentions.None);
+            }
+        }
+
+        [NeedsDatabaseEntry]
+        [EnabledInDm(false)]
+        [DefaultMemberPermissions(GuildPermission.ManageGuild)]
+        [SlashCommand("set-main-outfit", "Sets the the main outfit represented by this server")]
+        public async Task SetMainOutfit(
+            [Summary(description: "The tag of the outfit")]
+            string outfitTag)
+        {
+            await DeferAsync();
+
+            var guild = _guildDb.Guilds.FindAsync(Context.Guild.Id);
+
+            var outfitCountJson = await _httpClient.GetStringAsync($"http://census.daybreakgames.com/s:{_configuration.GetConnectionString("CensusAPIKey")}/count/ps2/outfit/?alias_lower={outfitTag.ToLower()}");
+            int? count = JObject.Parse(outfitCountJson)["count"]?.ToObject<int>();      //  Only the number of results is returned by the query. If the result is 1 it is assumed the given outfit exists, though it might be different from what the user requested
+            if (count == 0)
+            {
+                _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "No outfit found with tag {OutfitTag}", outfitTag);
+                await FollowupAsync($"No outfit found with tag {outfitTag}!");
+                return;
+            }
+            else if (!count.HasValue || count != 1)
+            {
+                _logger.SendLog(LogEventLevel.Warning, Context.Guild.Id, "Something went wrong requesting outfit tag {OutfitTag} from Census. Dumping JSON as a debug log message", outfitTag);
+                _logger.SendLog(LogEventLevel.Debug, Context.Guild.Id, "Census returned after requesting outfit tag {OutfitTag}:\n{json}", outfitTag, outfitCountJson);
+                await FollowupAsync($"Something went wrong while validating outfit tag {outfitTag}...");
+                return;
+            }
+
+            await FollowupAsync($"Main outfit set to {outfitTag}");
+            _logger.SendLog(LogEventLevel.Information, Context.Guild.Id, "Main outfit set to {OutfitTag}", outfitTag);
+
+            (await guild)!.OutfitTag = outfitTag;
+            _guildDb.SaveChanges();
+        }
+    }
+}
--- PS2 Assistant/Modules/SlashCommands/SlashCommands.cs
+++ PS2 Assistant/Modules/SlashCommands/SlashCommands.cs
@@ -1,9 +1,26 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
+using PS2_Assistant.Data;
+using PS2_Assistant.Logger;
 
 namespace PS2_Assistant.Modules.SlashCommands
 {
     public partial class SlashCommands : InteractionModuleBase<SocketInteractionContext>
     {
+        private readonly BotContext _guildDb;
+        private readonly SourceLogger _logger;
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
+        private const ChannelPermission _channelWritePermissions = ChannelPermission.ViewChannel | ChannelPermission.SendMessages;
+
+        public SlashCommands(BotContext guildDb, SourceLogger logger, HttpClient httpClient, IConfiguration configuration)
+        {
+            _guildDb = guildDb;
+            _logger = logger;
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
     }
 }
