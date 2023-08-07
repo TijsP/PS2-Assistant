6648286cce7bd238767b18dd5279c7920f5c1bc8
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-08-07 15:10:25 +0200

    Implemented help setup command in IF

--- PS2 Assistant/Modules/SlashCommands/SlashCommands.Basic.cs
+++ PS2 Assistant/Modules/SlashCommands/SlashCommands.Basic.cs
@@ -51,7 +51,45 @@ namespace PS2_Assistant.Modules.SlashCommands
             [SlashCommand("setup", "Details how to set the bot up on this server")]
             public async Task Setup()
             {
-                await RespondAsync("Displaying setup page");
+                var embeds = new List<Embed>(){
+                        new EmbedBuilder()
+                        .WithTitle("Configure Channels")
+                        .WithDescription("First of all, make sure all channels are configured properly. Use `/set-welcome-channel` to set a welcome channel, and use `/set-log-channel` to set a log channel. " +
+                                            "The welcome channel will be used to send messages whenever a new user joins (such as a welcome message, or a nickname poll), while the log channel will be used to send " +
+                                            "messages that are of interest to the admins. Because of this, it's recommended to set the welcome channel to a public channel, and the log channel to a private (admin only) " +
+                                            "channel. While a welcome channel is optional, it's highly recommended to set up a log channel ASAP.\n" +
+                                            "Also, please make sure the bot has the right permissions to post in these channels - namely the \"View Channel\" and \"Send Messages\" permissions")
+                        .WithColor(247, 82, 37)
+                        .Build(),
+                    new EmbedBuilder()
+                        .WithTitle("Set Main Outfit")
+                        .WithDescription("Next, please inform the bot of the main outfit represented by this server by using `/set-main-outfit`. If left unset, all users that join will be given the non-member role " +
+                                            "(see the next step). The provided tag will be checked against the Planetside API, to ensure the outfit actually exists.")
+                        .WithColor(247, 82, 37)
+                        .Build(),
+                    new EmbedBuilder()
+                        .WithTitle("Nickname Poll")
+                        .WithDescription("Now we'll set up the behaviour of the nickname poll. This poll can ask the user for their in-game character name and, if the character exists, will set their Discord nickname to " +
+                                            "their outfit tag + their character name (for example, \"[OUTF] xXCharacterNameXx\"). Using `/include-nickname-poll`, you can choose whether this poll should be sent whenever a new " +
+                                            "user joins, while `/send-nickname-poll` will send a nickname poll to whichever channel the bot has access to (the rules channel, for instance).")
+                        .WithColor(247, 82, 37)
+                        .Build(),
+                    new EmbedBuilder()
+                        .WithTitle("Configure Roles")
+                        .WithDescription("Now we're ready to configure the roles that will be handed out by the bot. These can be set by `/set-member-role` and `/set-non-member-role`. The member role will be given whenever " +
+                                            "the in-game character of the user is a member of the outfit specified by `/set-main-outfit`, while the non-member role will be given in any other case.\n" +
+                                            "Please keep in mind that this bot has no way of actually verifying whether the user actually owns the character provided: for this reason, it's recommended not to hand out any " +
+                                            "roles that have meaningfull permissions associated with them.\n" +
+                                            "Also, make sure the role of the bot outranks any of the roles set by `/set-member-role` and `/set-non-member-role`. The bot will not be able to hand out these roles otherwise.")
+                        .WithColor(247, 82, 37)
+                        .Build(),
+                    new EmbedBuilder()
+                        .WithTitle("Optionally")
+                        .WithDescription("Finally, you can choose whether the bot should send a general welcome message whenever a user joins. This can be done using `/send-welcome-message`. Welcome messages will be sent to " +
+                                            "the channel specified with `/set-welcome-channel`.")
+                        .WithColor(247, 82, 37)
+                        .Build()};
+                await RespondAsync(embeds: embeds.ToArray());
             }
 
             [SlashCommand("command", "Get help for a specific command")]
