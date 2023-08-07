5d94d26fa48cb1a00dfe32c87635b11f49214eac
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-08-07 15:20:13 +0200

    Made Guild Channels and Roles non-nullable

--- PS2 Assistant/Models/Guild.cs
+++ PS2 Assistant/Models/Guild.cs
@@ -6,8 +6,8 @@ public class Guild
     [Key]
     public ulong GuildId { get; set; }
 
-    public Channels? Channels { get; set; }
-    public Roles? Roles { get; set; }
+    public Channels Channels { get; set; } = new Channels();
+    public Roles Roles { get; set; } = new Roles();
     public ICollection<User> Users { get; } = new List<User>();
     public string? OutfitTag { get; set; }
     public bool AskNicknameUponWelcome { get; set; } = false;
