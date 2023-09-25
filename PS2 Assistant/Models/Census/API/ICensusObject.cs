8a22fbb634542f1f035a8f94cdc43cf40880a2e5
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-09-25 16:46:17 +0200

    Updated description

--- PS2 Assistant/Models/Census/API/ICensusObject.cs
+++ PS2 Assistant/Models/Census/API/ICensusObject.cs
@@ -1,12 +1,12 @@
 ﻿namespace PS2_Assistant.Models.Census.API
 {
     /// <summary>
-    /// generic
+    /// Contains the collection and the query required to get the desired data.
     /// </summary>
     interface ICensusObject
     {
         /// <summary>
-        /// Contains the collection and the query required to get the desired data. Any specifiers can be added to the end of this string, i.e. <example>"&name.first_lower=name"</example>
+        /// Any specifiers can be added to the end of this string, i.e. <example>"&name.first_lower=name"</example>
         /// </summary>
         public static abstract string CollectionQuery { get; }
     }
