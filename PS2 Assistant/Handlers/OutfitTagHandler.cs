c011761638314c16dc3ca258be971349bfc6b3d0
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-09-28 16:38:16 +0200

    Refactored RegisteredUsersByOutfit to take a Guild as a parameter instead of a guild id

--- PS2 Assistant/Handlers/OutfitTagHandler.cs
+++ PS2 Assistant/Handlers/OutfitTagHandler.cs
@@ -17,21 +17,15 @@ namespace PS2_Assistant.Handlers
         }
 
         /// <summary>
-        /// Queries the database for all registered users within a guild
+        /// Sorts all registered users in <paramref name="guild"/> by outfit
         /// </summary>
-        /// <param name="guildId">The Id of the guild for which to generate the Dictionary</param>
-        /// <returns>Null when no database entry was found for the guild, or a Dictionary containing a list of registered users grouped by outfit, with the outfit tag as the key ("" if unaffiliated)</returns>
-        private async Task<Dictionary<string, List<User>>?> RegisteredUsersByOutfit(ulong guildId)
-        {
-            if (await _guildDb.GetGuildByGuildIdAsync(guildId) is not Guild guild)
+        /// <param name="guild">The database entry of the guild for which to generate the Dictionary</param>
+        /// <returns>A <see cref="Dictionary{TKey, TValue}"/> containing a list of registered users grouped by outfit, with the outfit tag as the key ("" if unaffiliated)</returns>
+        private Dictionary<string, List<User>> RegisteredUsersByOutfit(Guild guild)
             {
-                _logger.SendLog(Serilog.Events.LogEventLevel.Error, guildId, "No database entry found for this guild");
-                return null;
-            }
-
             //  Each key represents an outfit tag ("" for no outfit), corresponding to a list of users with that tag
             Dictionary<string, List<User>> registeredOutfits = guild.Users.GroupBy(x => x.CurrentOutfit ?? "").ToDictionary(x => x.Key, y => y.ToList());
-            _logger.SendLog(Serilog.Events.LogEventLevel.Debug, guildId, "Registered players grouped by outfit:\n{GroupedUsers}", JsonConvert.SerializeObject(registeredOutfits, Formatting.Indented));
+            _logger.SendLog(LogEventLevel.Debug, guild.GuildId, "Registered players grouped by outfit:\n{GroupedUsers}", JsonConvert.SerializeObject(registeredOutfits, Formatting.Indented));
 
             return registeredOutfits;
         }
