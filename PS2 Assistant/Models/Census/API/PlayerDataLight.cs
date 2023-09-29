f5cf7c93db07c602793c7f5d352fe6b5b21747c7
Tijs Perdaems <Tijsperdaems@gmail.com>
2023-09-29 16:46:32 +0200

    Added alias_lower to outfit_member_extended join

--- PS2 Assistant/Models/Census/API/PlayerDataLight.cs
+++ PS2 Assistant/Models/Census/API/PlayerDataLight.cs
@@ -10,6 +10,6 @@
         /// <summary>
         /// Returns 1 by default. Append <code>&c:limit=X</code> to get more results. Append <code>&name.first_lower=*</code> for a specific character
         /// </summary>
-        public static string CollectionQuery => "character_name/?c:join=outfit_member_extended^on:character_id^inject_at:outfit^show:alias&c:exactMatchFirst=true";
+        public static string CollectionQuery => "character_name/?c:join=outfit_member_extended^on:character_id^inject_at:outfit^show:alias'alias_lower&c:exactMatchFirst=true";
     }
 }
